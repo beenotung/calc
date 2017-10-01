c285d3aeeb0111f1c211f2b0b79bdb634be3bf95
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-10-01 18:48:48 +0800

    init commit

---
+++ calc.c
@@ -0,0 +1,8 @@
+// string based calculator
+#include <stdio.h>
+#define noop(...)
+#define debug printf
+int main(int argc, char** args) {
+  debug("there are %d arguments\n", argc - 1);
+  return 0;
+}
